07f52db3417d3de2d88a3285d9f039f2c4020f0f
Kazibwe Julius Jr <kazibwejuliusjunior@gmail.com>
2021-02-24 11:32:28 +0300

    SLIDER INFO

--- resources/views/kike/index.blade.php
+++ resources/views/kike/index.blade.php
@@ -4,6 +4,49 @@
 
 <div class="top-shadow"></div>
 <section class="home-slider owl-carousel">
+    <div class="slider-item" style="background-image: url('asset_files/imgz/IMG_0050-min.JPG');">
+        <div class="container">
+            <div class="row slider-text align-items-center justify-content-center">
+                <div class="col-lg-7 text-center col-sm-12 element-animate">
+                {{-- <div class="btn-play-wrap mx-auto"><p class="mb-4"><a href="https://vimeo.com/59256790" data-fancybox data-ratio="2" class="btn-play"><span class="ion ion-ios-play"></span></a></p></div> --}}
+                <h1 class="mb-4"><span>KIKE TROPICAL
+                    FRUITS</span></h1>
+                <p class="mb-5 w-75">Improving fruit farmer's household income through
+                    providing ready market for their fruits.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <div class="slider-item" style="background-image: url('asset_files/imgz/IMG_0016-min.JPG');">
+        <div class="container">
+            <div class="row slider-text align-items-center justify-content-center">
+                <div class="col-lg-7 text-center col-sm-12 element-animate">
+                {{-- <div class="btn-play-wrap mx-auto"><p class="mb-4"><a href="https://vimeo.com/59256790" data-fancybox data-ratio="2" class="btn-play"><span class="ion ion-ios-play"></span></a></p></div> --}}
+                <h1 class="mb-4"><span>KIKE TROPICAL
+                    FRUITS</span></h1>
+                <p class="mb-5 w-75">Improving fruit farmer's household income through
+                    providing ready market for their fruits.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="slider-item" style="background-image: url('asset_files/imgz/IMG_0079-min.JPG');">
+        <div class="container">
+            <div class="row slider-text align-items-center justify-content-center">
+                <div class="col-lg-7 text-center col-sm-12 element-animate">
+                {{-- <div class="btn-play-wrap mx-auto"><p class="mb-4"><a href="https://vimeo.com/59256790" data-fancybox data-ratio="2" class="btn-play"><span class="ion ion-ios-play"></span></a></p></div> --}}
+                <h1 class="mb-4"><span>KIKE TROPICAL
+                    FRUITS</span></h1>
+                <p class="mb-5 w-75">Improving fruit farmer's household income through
+                    providing ready market for their fruits.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
 <div class="slider-item" style="background-image: url('/asset_files/imgz/IMG_0061-min.JPG');">
 <div class="container">
 <div class="row slider-text align-items-center justify-content-center">
@@ -18,18 +61,19 @@
 </div>
 </div>
 <div class="slider-item" style="background-image: url('asset_files/imgz/IMG_0050-min.JPG');">
-<div class="container">
-<div class="row slider-text align-items-center justify-content-center">
-<div class="col-lg-7 text-center col-sm-12 element-animate">
-{{-- <div class="btn-play-wrap mx-auto"><p class="mb-4"><a href="https://vimeo.com/59256790" data-fancybox data-ratio="2" class="btn-play"><span class="ion ion-ios-play"></span></a></p></div> --}}
-<h1 class="mb-4"><span>KIKE TROPICAL
-    FRUITS</span></h1>
-<p class="mb-5 w-75">Improving fruit farmer's household income through
-    providing ready market for their fruits.</p>
+    <div class="container">
+        <div class="row slider-text align-items-center justify-content-center">
+            <div class="col-lg-7 text-center col-sm-12 element-animate">
+            {{-- <div class="btn-play-wrap mx-auto"><p class="mb-4"><a href="https://vimeo.com/59256790" data-fancybox data-ratio="2" class="btn-play"><span class="ion ion-ios-play"></span></a></p></div> --}}
+            <h1 class="mb-4"><span>KIKE TROPICAL
+                FRUITS</span></h1>
+            <p class="mb-5 w-75">Improving fruit farmer's household income through
+                providing ready market for their fruits.</p>
+            </div>
+        </div>
+    </div>
 </div>
-</div>
-</div>
-</div>
+
 </section>
 
 </div>
